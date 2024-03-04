d227af2c718d1d6a5413e060ce9d00e1e4f2186d
HANNIA123 <hannia23413@gmail.com>
2024-03-03 22:46:11 -0600

    Consulta vehiculo

---
+++ app/src/main/java/com/example/curdfirestore/Usuario/Conductor/ConsultasConductor/obtenerVehiculo.kt
@@ -0,0 +1,40 @@
+package com.example.curdfirestore.Usuario.Conductor.ConsultasConductor
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.avanti.UserData
+import com.example.avanti.Usuario.RetrofitClientUsuario
+import com.example.avanti.VehicleData
+
+
+@Composable
+fun conObtenerVehiculoId(correo: String): VehicleData? {
+    var fin by remember {
+        mutableStateOf(false)
+    }
+    var vehiculo by remember { mutableStateOf<VehicleData?>(null) }
+
+    LaunchedEffect(key1 = true) {
+        try {
+            val resultadoVehiculo = RetrofitClientUsuario.apiService.obtenerVehiculo(correo)
+            vehiculo = resultadoVehiculo
+            // Haz algo con el objeto Usuario
+
+        } catch (e: Exception) {
+            println("Error al obtener vehiculo: $e")
+        } finally {
+            fin = true
+        }
+    }
+
+    // Puedes retornar el usuario directamente
+    return if (fin) {
+        vehiculo
+    } else {
+        null
+    }
+}
