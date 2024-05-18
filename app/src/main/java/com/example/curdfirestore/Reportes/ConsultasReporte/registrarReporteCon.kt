65b9777b343e9c52b41f0801093395ed3bd391de
HANNIA123 <hannia23413@gmail.com>
2024-05-18 14:17:39 -0600

    cambio envio de reporte

--- app/src/main/java/com/example/curdfirestore/Reportes/ConsultasReporte/registrarReporteCon.kt
+++ app/src/main/java/com/example/curdfirestore/Reportes/ConsultasReporte/registrarReporteCon.kt
@@ -6,12 +6,10 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
 
 fun conRegistrarReporte(reporteData: ReporteData) {
-    // Obtener una referencia a la base de datos de Firebase
-    val db: FirebaseFirestore = Firebase.firestore
 
-    // Crear una referencia a la colección "viajes"
+    val db: FirebaseFirestore = Firebase.firestore
     val viajesRef = db.collection("reporte")
-    // Agregar el objeto Viaje a la colección con un ID generado automáticamente
+
     viajesRef.add(reporteData)
         .addOnSuccessListener { documentReference ->
             println("Viaje agregado correctamente a Firebase con ID: ${documentReference.id}")
